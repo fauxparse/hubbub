fe9f2984eae32a122b99255000d5406a700c6acc
Matt Powell <fauxparse@gmail.com>
2009-01-17 08:32:30 +1300

    upgrades to statefulness

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,11 @@
 class Task < ActiveRecord::Base
   belongs_to :task_list
+  has_many :assignments
+  has_many :blockages, :through => :assignments
   
   include Statefulness
+  
+  def can_complete?
+    assignments.inject(true) { |t, a| t && a.completed? }
+  end
 end
--- lib/statefulness.rb
+++ lib/statefulness.rb
@@ -5,21 +5,47 @@ module Statefulness
   def self.included(base)
     base.instance_eval do
       include Stateful
+      
       stateful :current do
-        state :active do |state|
+        state :open do |state|
           state.event :complete, :to => :completed
           state.event :delete, :to => :deleted
         end
         state :completed do |state|
+          state.event :reopen, :to => :open
           state.event :delete, :to => :deleted
         end
-        state :deleted
+        state :deleted do |state|
+          # TODO save pre-deletion state somewhere?
+          state.event :undelete, :to => :open
+        end
+        
+        each_event do |event|
+          event.requires :valid?
+          event.requires(:permission) do |t|
+            method_name = :"can_#{event.name}?"
+            !t.receiver.respond_to?(method_name) || (t.receiver.send(method_name) != false)
+          end
+          event.execute do |t|
+            method_name = :"on_#{event.name}"
+            t.receiver.send method_name if t.receiver.respond_to? method_name
+          end
+          event.after :save!
+        end
       end
+    end
+    
+    # These method definitions are needed so that event.requires and event.after can function correctly above.
+    %w(valid? save!).each do |method_name|
+      method_name =~ /^([^\!\?]*)([\?\!]?)$/
+      with, without = :"#{$1}_with_statefulness#{$2}", :"#{$1}_without_statefulness#{$2}"
+      base.send(:define_method, with) { |*args| send without }
+      base.send :alias_method_chain, method_name, :statefulness
     end
   end
   
-  def active?
-    currently? :active
+  def open?
+    currently? :open
   end
   
   def completed?
