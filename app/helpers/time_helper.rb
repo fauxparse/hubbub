69551d3e16ef4f44d8e9cffddb1e07dfee87ed9b
Matt Powell <fauxparse@gmail.com>
2009-02-11 12:30:27 +1300

    time graph

--- app/helpers/time_helper.rb
+++ app/helpers/time_helper.rb
@@ -3,6 +3,7 @@ module TimeHelper
     end_date ||= Date.today
     start_date = end_date - 1.month
     returning "" do |result|
+      result << "<div class=\"graph\">"
       result << content_tag(:h2, describe_date_range(start_date, end_date, :include_day => false))
       result << "<ul class=\"timeline\">"
       dates = (start_date..end_date).to_a
@@ -11,9 +12,11 @@ module TimeHelper
       max_hours = [ hours.max.to_i, 8 * 60 ].max
       
       hours_per_day.each do |d, h|
-        result << content_tag(:li, link_to(content_tag(:span, d.day, :class => :label) + content_tag(:span, "(#{h.to_s(:fraction)})", :class => :count, :style => "height:#{h.to_i * 100 / max_hours}%"), "#", :title => d.to_s))
+        result << content_tag(:li, link_to(content_tag(:span, d.day, :class => :label) + content_tag(:span, "(#{h.to_s(:fraction)})", :class => :count, :style => "height:#{h.to_i * 100 / max_hours}%"), "#", :title => d.to_s), :class => ((d.wday + 1) % 7 < 2 ? "weekend" : "weekday"))
       end
       result << "</ul>"
+      result << clear_floats
+      result << "</div>"
     end
   end
   
