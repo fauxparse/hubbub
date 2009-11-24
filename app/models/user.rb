4ba306ab73f06f4b9995ee943cff44add08d0194
Matt Powell <fauxparse@gmail.com>
2009-11-25 09:43:59 +1300

    update to match current gems

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  acts_as_authentic :scope => :account_id
+  acts_as_authentic
   belongs_to :account
   belongs_to :company
   has_and_belongs_to_many :roles, :uniq => true
